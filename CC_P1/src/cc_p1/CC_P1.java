c8dd8a38a30fa506a64f076c513d58a0cb327b8b
Your Name <you@example.com>
2017-11-03 09:39:57 +0100

    initial p1

---
+++ CC_P1/src/cc_p1/CC_P1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc_p1;
+
+/**
+ *
+ * @author matti
+ */
+public class CC_P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
