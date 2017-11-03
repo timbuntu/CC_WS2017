e703adb2389f7e81da2380bd99584d30b8365708
Your Name <you@example.com>
2017-11-03 12:44:38 +0100

    simple test main

--- CC_P1/src/cc_p1/CC_P1.java
+++ CC_P1/src/cc_p1/CC_P1.java
@@ -5,6 +5,16 @@
  */
 package cc_p1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import cc_p1.Exceptions.*;
+
 /**
  *
  * @author matti
@@ -16,6 +26,29 @@ public class CC_P1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        List<String> symbolTable = new ArrayList<>();
+
+        if (args.length < 1) {
+            System.exit(1);
+        }
+
+        String file = args[0];
+        System.out.println(System.getenv("PWD"));
+        InputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CC_P1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Lexer lexer = new Lexer(fileStream, symbolTable);
+        try {
+            while (lexer.getToken().getType() != Token.TokenType.EOF) {
+            }
+        } catch (UnknownLexemeException ex) {
+            Logger.getLogger(CC_P1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
